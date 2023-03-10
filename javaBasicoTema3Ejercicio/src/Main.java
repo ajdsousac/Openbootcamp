7ed57741c429e8133e0a5482eeb591a1d71e7cda
Adrián De Sousa <112829943+ajdsousac@users.noreply.github.com>
2023-03-10 12:53:46 -0400

    Add files via upload
    Ejercicio Tema 3 del curso JavaBasico de Openbootcamp

---
+++ javaBasicoTema3Ejercicio/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] diasSemana = {"Domingo", "Lunes", "martes", "Miercoles", "Jueves", "Viernes", "Sábado"};
+
+        for (int i = 0; i < diasSemana.length; i++) {
+            System.out.println(diasSemana[i]);
+        }
+    }
+}
